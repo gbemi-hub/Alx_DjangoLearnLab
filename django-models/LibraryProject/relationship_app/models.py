445440b1bfbaccb8a814faeef40c265b986149ca
Amodu Oreoluwa <amoduoreoluwa20@gmail.com>
2025-11-15 20:08:03 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+Class Author(models.Model)
+  name = models.CharField(max_length=100)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
